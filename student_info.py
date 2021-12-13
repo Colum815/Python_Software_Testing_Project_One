7771b06eabb983552e34235294bffa8c46a7ba6b
Colum <56079750+Cullie815@users.noreply.github.com>
2021-12-13 12:25:59 +0000

    Update student_info.py

--- student_info.py
+++ student_info.py
@@ -1,16 +1,16 @@
 """
 First I have created a class with a constructor inside it with three parameters to be used through out the class.
-When an object of the class is created it will have to contain the three arguments.
+When an object of the class is created it will have to contain three arguments.
 Next I have three basic functions:
 1:The first 'def display_name' returns the first and last name entered.
 2:The second function 'def display_email' returns the first and last name and adds '@yahoo.ie'.
   It then makes all the letters lowercase.
-3:The third function 'def display_average_score' returns the average of the grades entered in the object of the class.
-I then added the @property decorator on all the functions so the functions become properties of the class.
+3:The third function 'def display_average_score' returns the average of the grades entered in the object of the class StudentInfo.
+I then added the @property decorator on all the functions so the functions become properties of the class instead.
 I can do this because each function only has one parameter 'self'.
 Next I created an object of the class and stored it in student_one. I passed three arguments to it
 and printed the various properties to the console just to make sure everything was working.
-I have commented out this part and no deleted it so that anyone can quickly see that everything is working so far.
+I have commented out this part and not deleted it so that anyone can quickly see that everything is working so far if they want to check.
 Now its over to the test_student_info.py file to carry out the testing.
 """
 
